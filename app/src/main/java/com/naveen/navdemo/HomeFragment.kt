9dfa4e9c37565028c0ec49e0f30109d73efff60e
thephotogenicbug <navinkumarachari@gmail.com>
2022-09-17 17:54:34 +0530

    SetonClickListener to Navigate from Home to Second Fragment

--- app/src/main/java/com/naveen/navdemo/HomeFragment.kt
+++ app/src/main/java/com/naveen/navdemo/HomeFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
 import com.naveen.navdemo.databinding.ActivityMainBinding
 import com.naveen.navdemo.databinding.FragmentHomeBinding
 
@@ -15,7 +16,9 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentHomeBinding.inflate(inflater,container, false)
-        // Inflate the layout for this fragment
+        binding.btnSubmit.setOnClickListener {
+            it.findNavController().navigate(R.id.action_homeFragment_to_secondFragment)
+        }
         return binding.root
     }
 }
