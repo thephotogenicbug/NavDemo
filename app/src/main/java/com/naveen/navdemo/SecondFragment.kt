67c141ed5a76a0910beea81d133c66fc8e61f3f1
thephotogenicbug <navinkumarachari@gmail.com>
2022-09-17 17:50:09 +0530

    Enable ViewBinding For SecondFragment

--- app/src/main/java/com/naveen/navdemo/SecondFragment.kt
+++ app/src/main/java/com/naveen/navdemo/SecondFragment.kt
@@ -5,15 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.naveen.navdemo.databinding.FragmentSecondBinding
 
 class SecondFragment : Fragment() {
-
+    private lateinit var binding : FragmentSecondBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_second, container, false)
+        binding = FragmentSecondBinding.inflate(inflater,container,false)
+        return binding.root
     }
 
 }
